8a119bd107fc924d73c3e84751b4c3f78bc9dc11
Erik R. Jensen <ejensen@netradius.com>
2018-01-30 20:23:12 -0700

    Adding constructors to ValidationError

--- netradius-spring-errors-spring-boot-starter/src/main/java/com/netradius/spring/errors/web/ValidationError.java
+++ netradius-spring-errors-spring-boot-starter/src/main/java/com/netradius/spring/errors/web/ValidationError.java
@@ -1,8 +1,12 @@
 package com.netradius.spring.errors.web;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ValidationError {
   private String field;
   private String message;
